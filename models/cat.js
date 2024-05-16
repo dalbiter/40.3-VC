64c295f8b95b5e1ec9eba19a3276979c3d9cfb32
danx <rushn24@gmail.com>
2024-05-16 19:34:04 -0400

    finished cat routes

--- models/cat.js
+++ models/cat.js
@@ -1,17 +1,62 @@
 const db = require("../db");
 const ExpressError = require('../expressError')
 
+// 40.3.3 Simple OO approach part 1
 class Cat {
   static async getAll() {
     let result = await db.query("SELECT id, name, age FROM cats")
     return result.rows;
-  }
+  };
 
   static async getById(id) {
     let result = await db.query("SELECT id, name, age FROM cats WHERE id=$1", [id])
     if(result.rows.length === 0) throw new ExpressError('Cat not found', 404)
     return result.rows[0];
-  }
-}
+  };
+
+// 40.3.4 Simple OO approach part 2
+  static async create(name, age) {
+    if (!name || !age) throw new ExpressError("Missing require data!", 400)
+    let result = await db.query(
+      `INSERT INTO cats (name, age) 
+      VALUES ($1, $2)
+      RETURNING id, name, age`, [name, age]
+    );
+    return result.rows[0];
+  };
+
+  static async delete(id) {
+    let result = await db.query(
+      `DELETE FROM cats
+      WHERE id=$1
+      RETURNING id, name, age`, 
+      [id]
+    )
+    if (result.rows.length === 0) throw new ExpressError("Cat not found", 404)
+    return result.rows[0]
+  };
+
+// 40.3.5 Simple OO approach part 3
+  static async update(id, name, age) {
+    const result = await db.query(`
+    UPDATE cats SET name=$1, age=$2
+    WHERE id=$3
+    RETURNING id, name, age`,
+    [name, age, id])
+    if (result.rows.length === 0) throw new ExpressError("Cat not found", 404)
+    return result.rows[0]
+  };
+
+  static async makeOlder(id) {
+    const result = await db.query(`
+    UPDATE cats SET age=age+1
+    WHERE id=$1
+    RETURNING id, name, age`,
+    [id])
+    if (result.rows.length === 0) throw new ExpressError("Cat not found", 404)
+    return result.rows[0]
+  };
+
+};
 
 module.exports = Cat;
--- routes/cats.js
+++ routes/cats.js
@@ -4,7 +4,7 @@ const db = require("../db");
 
 const router = new express.Router();
 
-
+// 40.3.3 Simple OO approach part 1
 /** get all cats: [{id, name, age}, ...] */
 router.get("/", async function (req, res, next) {
   try {
@@ -22,7 +22,46 @@ router.get('/:id', async (req, res, next) => {
   } catch(e) {
     return next(e)
   }
+});
+
+// 40.3.4 Simple OO approach part 2
+router.post('/', async (req, res, next) => {
+  try {
+    const { name, age } = req.body;
+    const newCat = await Cat.create(name, age)
+    return res.json(newCat)
+  } catch(e) {
+    return next(e)
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try{
+    await Cat.delete(req.params.id)
+    return res.json({ message: "Cat deleted" })
+  } catch(e) {
+    return next(e)
+  }
+});
+
+// 40.3.5 Simple OO approach part 3
+router.put('/:id', async (req, res, next) => {
+  try {
+    const { name, age } = req.body;
+    const cat = await Cat.update(req.params.id, name, age) 
+    return res.json(cat) 
+  } catch(e) {
+    return next(e)
+  }
+});
+
+router.patch('/:id', async (req, res, next) => {
+  try {
+    const cat = await Cat.makeOlder(req.params.id) 
+    return res.json(cat) 
+  } catch(e) {
+    return next(e)
+  }  
 })
 
 module.exports = router;
-
